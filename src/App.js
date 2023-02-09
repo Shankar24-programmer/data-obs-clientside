920e219178b20c2d44e0ca3b30064a0ebb85b148
Muthu Sankar <reachtoshankarsk@gmail.com>
2023-02-09 12:54:36 +0530

    data-observability-ui

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sample from './components/Sample'
+
+function App() {
+  return (
+    <div>
+      <Sample />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sample.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Sample() {
+     return (
+          <>
+
+               <h3>This is sample page</h3>
+          </>
+     )
+}
